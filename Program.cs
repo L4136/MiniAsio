de9342a9cbc9cd1e09bb4ea24e8f1925758ed164
Heli Vepsäläinen <L4136@student.jamk.fi>
2018-02-10 12:18:02 +0200

    L09T01

--- Program.cs
+++ Program.cs
@@ -5,10 +5,46 @@ using System.Text;
 
 namespace MiniAsio
 {
+    class Student : IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AsioId { get; set; }
+        public string ClassId { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + AsioId + " " + ClassId;
+        }
+        public int CompareTo(object obj)
+        {
+            Student compareTo = (Student)obj;
+            int lastnames = LastName.CompareTo(compareTo.LastName);
+            if (lastnames == 0) return FirstName.CompareTo(compareTo.FirstName);
+            return lastnames;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Dictionary<string, Student> students = new Dictionary<string, Student>();
+        
+            Student student1 = new Student { FirstName = "Masa", LastName = "Niemi", AsioId = ""};
+            Student student2 = new Student { FirstName = "Allan", LastName = "Aalto", AsioId = "A1111" };
+            Student student3 = new Student { FirstName = "Hanna", LastName = "Husso", AsioId = "B2222" };
+            Student student4 = new Student { FirstName = "Teppo", LastName = "Testaaja", AsioId = "C3333", ClassId = "TTV17S4" };
+
+            students.Add(student1.AsioId, student1);
+            students.Add(student2.AsioId, student2);
+            students.Add(student3.AsioId, student3);
+            students.Add(student4.AsioId, student4);
+
+            foreach (Student student in students.Values)
+            {
+                Console.WriteLine(student.ToString());
+            }
         }
     }
 }
